c6a12a72e7168e05cbdea0dbda0c579393ccab13
elisa236164 <s236164@dtu.dk>
2024-04-26 10:34:47 +0200

    commits

--- src/carType.java
+++ src/carType.java
@@ -1,3 +1,2 @@
-public interface carType {
-
+public class carType {
 }
--- src/sportsCar.java
+++ src/sportsCar.java
@@ -1,4 +1,45 @@
-public class sportsCar implements carType{
-    private Strin
+public class sportsCar extends carType{
 
+    private boolean automaticGear;
+    private boolean airConditon;
+
+
+
+    private boolean cruiseControl;
+
+
+
+    private int leatherSeat;
+
+
+    public sportsCar(boolean automaticGear, boolean airConditon, boolean cruiseControl, int leatherSeat) {
+        this.automaticGear = automaticGear;
+        this.airConditon = airConditon;
+        this.cruiseControl = cruiseControl;
+        this.leatherSeat = leatherSeat;
+    }
+
+    public boolean getAirConditon() {
+        return airConditon;
+    }
+
+    public void setAirConditon(boolean airConditon) {
+        this.airConditon = airConditon;
+    }
+
+    public int getLeatherSeat() {
+        return leatherSeat;
+    }
+
+    public void setLeatherSeat(int leatherSeat) {
+        this.leatherSeat = leatherSeat;
+    }
+
+    public boolean isCruiseControl() {
+        return cruiseControl;
+    }
+
+    public void setCruiseControl(boolean cruiseControl) {
+        this.cruiseControl = cruiseControl;
+    }
 }
